4e5ad9051f68d0829f9309d76db16716e72edc0e
Maxwell Koo <mjkoo90@gmail.com>
2019-12-05 17:16:45 -0700

    Clean up day 5 python a bit

--- day5/python/main.py
+++ day5/python/main.py
@@ -3,87 +3,106 @@ import sys
 import operator
 
 
-def decode(opcode):
-    d = 10000
-    m3, opcode, d = opcode // d, opcode % d, d // 10
-    m2, opcode, d = opcode // d, opcode % d, d // 10
-    m1, opcode, d = opcode // d, opcode % d, d // 10
-    return (m1, m2, m3, opcode)
+class Intcode:
+    def __init__(self, mem, inputs):
+        self.mem = mem
+        self.pc = 0
+        self.inputs = inputs
+        self.outputs = []
+
+    def run(self):
+        while True:
+            opcode = self.mem[self.pc]
+            ins = Instruction(opcode)
+            off = ins.run(self)
+            if off is None:
+                break
+            self.pc += off
+
+        return self.outputs
 
 
-def intcode(mem, inputs):
-    outputs = []
+class Instruction:
+    def __init__(self, opcode):
+        d = 10000
+        m3, opcode, d = opcode // d, opcode % d, d // 10
+        m2, opcode, d = opcode // d, opcode % d, d // 10
+        m1, opcode, d = opcode // d, opcode % d, d // 10
 
-    def load(mode, addr):
-        return addr if mode else mem[addr]
+        self.modes = [m1, m2, m3]
+        self.opcode = opcode
 
-    def store(addr, value):
-        mem[addr] = value
+    def load(self, m, arg):
+        addr = m.pc + arg + 1
+        return m.mem[addr] if self.modes[arg] else m.mem[m.mem[addr]]
 
-    def alu(m1, m2, m3, pc, op):
-        assert m3 == 0
-        a = load(m1, mem[pc + 1])
-        b = load(m2, mem[pc + 2])
-        store(mem[pc + 3], op(a, b))
+    def store(self, m, arg, value):
+        assert self.modes[arg] == 0
+        addr = m.mem[m.pc + arg + 1]
+        m.mem[addr] = value
+
+    def _alu(self, m, f):
+        a = self.load(m, 0)
+        b = self.load(m, 1)
+        self.store(m, 2, f(a, b))
         return 4
 
-    def add(m1, m2, m3, pc):
-        return alu(m1, m2, m3, pc, operator.add)
+    def add(self, m):
+        return self._alu(m, operator.add)
 
-    def mul(m1, m2, m3, pc):
-        return alu(m1, m2, m3, pc, operator.mul)
+    def mul(self, m):
+        return self._alu(m, operator.mul)
 
-    def lt(m1, m2, m3, pc):
-        return alu(m1, m2, m3, pc, lambda a, b: 1 if a < b else 0)
+    def lt(self, m):
+        return self._alu(m, lambda a, b: 1 if a < b else 0)
 
-    def eq(m1, m2, m3, pc):
-        return alu(m1, m2, m3, pc, lambda a, b: 1 if a == b else 0)
+    def eq(self, m):
+        return self._alu(m, lambda a, b: 1 if a == b else 0)
 
-    def jmp(m1, m2, m3, pc, cnd):
-        a = load(m1, mem[pc + 1])
-        b = load(m2, mem[pc + 2])
-        return b - pc if cnd(a) else 3
+    def _jmp(self, m, f):
+        a = self.load(m, 0)
+        b = self.load(m, 1)
+        return b - m.pc if f(a) else 3
 
-    def jnz(m1, m2, m3, pc):
-        return jmp(m1, m2, m3, pc, lambda a: a)
+    def jnz(self, m):
+        return self._jmp(m, lambda a: a)
 
-    def jz(m1, m2, m3, pc):
-        return jmp(m1, m2, m3, pc, lambda a: not a)
+    def jz(self, m):
+        return self._jmp(m, lambda a: not a)
 
-    def read(m1, m2, m3, pc):
-        assert m1 == 0
-        store(mem[pc + 1], inputs.pop())
+    def read(self, m):
+        self.store(m, 0, m.inputs.pop())
         return 2
 
-    def write(m1, m2, m3, pc):
-        a = load(m1, mem[pc + 1])
-        outputs.append(a)
+    def write(self, m):
+        a = self.load(m, 0)
+        m.outputs.append(a)
         return 2
 
-    def halt(m1, m2, m3, pc):
-        return -1
+    def halt(self, m):
+        return None
 
-    opcodes = {1: add, 2: mul, 3: read, 4: write, 5: jnz, 6: jz, 7: lt, 8: eq, 99: halt}
-
-    pc = 0
-    while True:
-        m1, m2, m3, opcode = decode(mem[pc])
-        op = opcodes[opcode]
-        off = op(m1, m2, m3, pc)
-
-        if off < 0:
-            break
-        pc += off
-
-    return outputs
+    def run(self, m):
+        operations = {
+            1: self.add,
+            2: self.mul,
+            3: self.read,
+            4: self.write,
+            5: self.jnz,
+            6: self.jz,
+            7: self.lt,
+            8: self.eq,
+            99: self.halt,
+        }
+        return operations[self.opcode](m)
 
 
 def main(argv):
     with open(argv[1], "r") as f:
         mem = [int(n) for n in f.read().split(",")]
 
-    print(intcode(mem.copy(), [1])[-1])
-    print(intcode(mem.copy(), [5])[-1])
+    print(Intcode(mem.copy(), [1]).run()[-1])
+    print(Intcode(mem.copy(), [5]).run()[-1])
 
 
 if __name__ == "__main__":
